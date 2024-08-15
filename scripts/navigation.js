cb71f83529ec6f91f5b1c10b0e884613de547edc
Eugene-Eyram-Ahiavey <phostechservices@gmail.com>
2024-08-15 23:24:16 +0000

    added js

---
+++ scripts/navigation.js
@@ -0,0 +1,14 @@
+const gh = document.getElementById('gh');
+gh.addEventListener("click", ()=>{
+    window.location.href = "./pages/ghana.html";
+});
+
+
+const back = document.getElementById("goBack");
+        back.addEventListener("click", ()=>{
+            window.location.href = "../index.html";
+        })
+
+ 
+
+
